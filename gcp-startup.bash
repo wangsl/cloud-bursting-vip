68a121c4eeaa7f0431fbb7f7c68579ab3a663a8b
wangsl <wangsl@users.noreply.github.com>
2025-10-23 18:35:41 -0400

    Update gcp-startup.bash for VIP 2025 fall

--- gcp-startup.bash
+++ gcp-startup.bash
@@ -21,10 +21,9 @@ sw77:x:1296493:1296493:Shenglong Wang:/home/sw77:/bin/bash
 wang:x:10015:10015:Shenglong Wang:/home/wang:/bin/bash
 wd35:x:2761180:2761180:Wensheng Deng:/home/wd35:/bin/bash 
 tc1841:x:2420783:2420783:Taidi Chen:/home/tc1841:/bin/bash 
-ama10047:x:3607771:3607771:Abdul Muqeet Agha:/home/ama10047:/bin/bash
-aia7143:x:4208823:4208823:Ayyaz Akhtar:/home/aia7143:/bin/bash
-pp2959:x:4216356:4216356:Preetham Rakshith Prakash:/home/pp2959:/bin/bash
-zw2686:x:3509154:3509154:Sid Wang:/home/zw2686:/bin/bash
+kc4207:*:3334730:3334730:Kevin Chan:/home/kc4207:/bin/bash
+np3129:*:4441975:4441975:Nishant Pushparaju:/home/np3129:/bin/bash
+zl4682:*:3912091:3912091:Ashley Liu:/home/zl4682:/bin/bash
 EOF
 
   unset IFS
