6cd357e51907f4eff22948a86bf84ab8c227347e
wangsl <wangsl@users.noreply.github.com>
2025-10-30 19:43:52 -0400

    Update gcp-startup.bash
     change * to x

--- gcp-startup.bash
+++ gcp-startup.bash
@@ -21,9 +21,9 @@ sw77:x:1296493:1296493:Shenglong Wang:/home/sw77:/bin/bash
 wang:x:10015:10015:Shenglong Wang:/home/wang:/bin/bash
 wd35:x:2761180:2761180:Wensheng Deng:/home/wd35:/bin/bash 
 tc1841:x:2420783:2420783:Taidi Chen:/home/tc1841:/bin/bash 
-kc4207:*:3334730:3334730:Kevin Chan:/home/kc4207:/bin/bash
-np3129:*:4441975:4441975:Nishant Pushparaju:/home/np3129:/bin/bash
-zl4682:*:3912091:3912091:Ashley Liu:/home/zl4682:/bin/bash
+kc4207:x:3334730:3334730:Kevin Chan:/home/kc4207:/bin/bash
+np3129:x:4441975:4441975:Nishant Pushparaju:/home/np3129:/bin/bash
+zl4682:x:3912091:3912091:Ashley Liu:/home/zl4682:/bin/bash
 EOF
 
   unset IFS
